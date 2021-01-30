884b7d54316008913dad28f18b22e003589faf84
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-01-31 02:31:56 +0400

    error handling on getWorkspaces and getWorkspaceInfo

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,24 +6,40 @@ const descriptionAPI = 'http://localhost:6060/api/workspace-description';
 const ratesAPI = 'http://localhost:4000/workspace-api/workspace';
 const photoAPI = '';
 
+// get list of workspaces near given id
 export const getWorkspaces = (url) => {
   return new Promise(async (resolve, reject) => {
-    const id = parseInt(url.split('/')[2]);
-    if (typeof id !== 'number') {
+    const extractId = url.split('/')[2];
+
+    const id = parseInt(extractId);
+
+    if (isNaN(extractId)) {
       resolve(false);
+      return;
     }
+
     try {
-      const { data: { nearbyWorkspaces }} = await axios.get(`${nearbyAPI}/buildings/${id}`);
-      resolve(nearbyWorkspaces);
+      const { data } = await axios.get(`${nearbyAPI}/buildings/${id}`);
+      if (data.success === false) {
+        resolve(false);
+      }
+      resolve(data.nearbyWorkspaces);
     } catch (error) {
+      console.log(error)
       reject(error);
     }
   })
-  .catch(error => console.log(error));
+  .catch(error => false);
 };
 
-export const getDescription = (id) => {
-  return axios.get(`${descriptionAPI}/${id}`);
+// getters for different services
+export const getDescription = async (id) => {
+  try {
+    const description = await axios.get(`${descriptionAPI}/${id}`);
+    return description;
+  } catch (error) {
+    return false;
+  }
 };
 
 export const getPhoto = (id) => {
@@ -42,27 +58,37 @@ export const getAmenities = async (id) => {
   });
 };
 
-export const getRates = (id) => {
-  return axios.get(`${ratesAPI}/${id}`);
+export const getRates = async (id) => {
+  try {
+    const rates = await axios.get(`${ratesAPI}/${id}`);
+    return rates;
+  } catch (error) {
+    return false;
+  }
 }
 
+// aggregate data from all getters into one object
 export const getWorkspaceInfo = (id) => {
   return new Promise( async (resolve, reject) => {
     try {
+      const info = {};
       const description = await getDescription(id);
+      info.description = description ? description.data : {};
+
       const photo = await getPhoto(id);
+      info.photo = photo ? photo.data : {};
+
       const amenities = await getAmenities(id);
+      info.amenities = amenities ? amenities.data : {};
+
       const rates = await getRates(id);
-      const info = {
-        description: description.data,
-        photo: photo.data,
-        amenities: amenities.data,
-        rates: rates.data[0]
-      };
+      info.rates = rates ? rates.data : {};
+
       resolve(info);
     } catch (error) {
+      console.log(error)
       reject(error);
     }
   })
-    .catch(err => err);
+    .catch(err => false);
 };
