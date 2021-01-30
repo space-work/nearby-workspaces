5043cbe3947580d20fb31cfc5983d5ba8f0b8a63
samasno <samasno@LAPTOP-HJT8OQVR.localdomain>
2021-01-31 02:32:33 +0400

    improved error handling and conditional rendering

--- client/src/components/workspace/Workspace.jsx
+++ client/src/components/workspace/Workspace.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import LoadingWorkspace from './LoadingWorkspace.jsx';
 import { getWorkspaceInfo } from '../../actions/';
 
+
 export default ({ location: { workspaceId, neighborhood }, details = null } ) => {
   const [space, setSpace] = useState(details);
+  // use passed in id to get details for given workspace
   if (space === null) {
+    console.log('fetching')
     getWorkspaceInfo(workspaceId)
       .then(res => setSpace(res))
       .catch(err => {
@@ -13,57 +16,61 @@ export default ({ location: { workspaceId, neighborhood }, details = null } ) =>
       })
   }
 
+  // loading and error cases
   if (space === null) {
     return (
       <LoadingWorkspace />
     )
   }
-
   if (space === false) {
-    return <div></div>
+    return <></>
   }
 
-  const { amenities: { amenities }, photo: { photo }, description, rates: { membership_rate } } = space;
+  const { amenities, photo, description, rates } = space;
 
-  if (!amenities || !photo || !description ) return <div></div>
+  // conditionally render array of amenities data
+  const Amenities = () => amenities.amenities ? (
+      <>
+      <li>{ amenities.amenities[0] }{' '}</li>
+          { amenities.amenities.slice(1).map(am => (
+          <li key={`${description.name}-${am}`}>&#8226; { am } </li> )
+          )}
+      </>
+    ) : <></>;
 
-  else {
-    return (
-      <div className="nb-container">
-        <a href={`http://localhost:5001/buildings/${workspaceId}`} className="light-text">
-          <div className="nb-grid">
-            <div className="nb-photo-container" style={{background: `url(${photo})`}}>
+  // insert any available data into workspace-card
+  return (
+    <div className="nb-container">
+      <a href={`/buildings/${ workspaceId }`} className="light-text">
+        <div className="nb-grid">
+          <div className="nb-photo-container" style={{background: `url(${photo.photo})`}}>
+          </div>
+          <div className="nb-description-container">
+            <div>
+              <h3 className="nb-description-title ">{ description.name || ''}</h3>
+              <p className="light-text bold-text">{ neighborhood || ''}</p>
             </div>
-            <div className="nb-description-container">
-              <div>
-                <h3 className="nb-description-title ">{ description.name }</h3>
-                <p className="light-text bold-text">{ neighborhood }</p>
-              </div>
-              <div className="light-text small-text bold-text">
-                <ul className="nb-amenities-list">
-                  <li>{ amenities[0] }{' '}</li>
-                  { amenities.slice(1).map(am => (
-                  <li key={`${description.name}-${am}`}>&#8226; { am } </li> )
-                  )}
-                </ul>
-              </div>
+            <div className="light-text small-text bold-text">
+              <ul className="nb-amenities-list">
+                <Amenities />
+              </ul>
             </div>
           </div>
-          <div className="nb-pricing-container dark-text">
-            <div className="nb-pricing-label bold-text pad-10">
-              <p>Available workspace</p>
-            </div>
-            <div className="nb-pricing-price pad-10">
-              { membership_rate ? (
-                  <p>from <span className="bolder-text">${ membership_rate }/mo</span></p>
-                ) : <p>View Inventory</p>
-              }
-            </div>
+        </div>
+        <div className="nb-pricing-container dark-text">
+          <div className="nb-pricing-label bold-text pad-10">
+            <p>Available workspace</p>
           </div>
-        </a>
-      </div>
-    );
-  }
+          <div className="nb-pricing-price pad-10">
+            { rates.membership_rate ? (
+                <p>from <span className="bolder-text">${ membership_rate }/mo</span></p>
+              ) : <p>View Inventory</p>
+            }
+          </div>
+        </div>
+      </a>
+    </div>
+  );
 }
 
   
