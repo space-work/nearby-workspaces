790b9c2048300688c457147fec65f2075d5017a1
Collin <collinmcmahon2014@yahoo.com>
2021-03-10 12:20:09 -0500

    testing mongoose connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,14 @@
 const path = require('path');
 require('dotenv').config();
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI_DEV, {
+connection.on('error', console.error.bind(console, 'mongoose connection error:'));
+
+connection.once('open', () => {
+  console.log('successfully connected to mongoose');
+});
+
+connect(process.env.MONGO_URI_DEV, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
